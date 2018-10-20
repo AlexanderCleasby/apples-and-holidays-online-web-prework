3a7dd3f8b9b0cdfca668540d93a0011926a21664
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-20 23:57:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each_key{|key|
-    Season_title=key+":"
+    season_title=key+":"
      key.class}
 end
 
