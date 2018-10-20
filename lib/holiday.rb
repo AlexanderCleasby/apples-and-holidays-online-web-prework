e2ffb054f4f1eec32393cd50a4828b85c97a9877
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-20 23:35:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def all_winter_holiday_supplies(holiday_hash)
   #  out.push(supp)}}
   #out
   holiday_hash[:winter].map{|holiday|  holiday.map{|supp| out.push(supp)}}
-  puts holiday_hash[:winter]
+  out
 end
 
 def all_supplies_in_holidays(holiday_hash)
