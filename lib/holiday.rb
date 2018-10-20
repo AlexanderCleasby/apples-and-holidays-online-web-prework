35a119d93716549337c3ae1087cd3a104daad197
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-20 23:44:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,20 +55,21 @@ def all_holidays_with_bbq(holiday_hash)
 
 end
 
-all_winter_holiday_supplies(
-  {
-    :winter => {
-      :christmas => ["Lights", "Wreath"],
-      :new_years => ["Party Hats"]
-    },
-    :summer => {
-      :fourth_of_july => ["Fireworks", "BBQ"]
-    },
-    :fall => {
-      :thanksgiving => ["Turkey"]
-    },
-    :spring => {
-      :memorial_day => ["BBQ"]
-    }
-  }
-)
+#all_winter_holiday_supplies(
+#  {
+#    :winter => {
+#      :christmas => ["Lights", "Wreath"],
+#      :new_years => ["Party Hats"]
+#    },
+#    :summer => {
+#      :fourth_of_july => ["Fireworks", "BBQ"]
+#    },
+#    :fall => {
+#      :thanksgiving => ["Turkey"]
+#    },
+#    :spring => {
+#      :memorial_day => ["BBQ"]
+#    }
+#  }
+#)
+#
