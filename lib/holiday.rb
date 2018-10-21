4e144aaee5cdd78ce6d265a453f2cd50a39af5cc
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 00:17:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each_key{|key|
     puts key.id2name[0].upcase+key.id2name[1...key.length]+":"
     holiday_hash[key].each_key{|holiday|
-      holiday_title = holiday.id2name.split("_").map{|word| word+"9"}
+      holiday_title = holiday.id2name.split("_").map{|word| word[0]}
       puts holiday_title
        }
 
