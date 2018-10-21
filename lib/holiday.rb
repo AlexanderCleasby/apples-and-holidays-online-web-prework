86b69e06615ebd19b892cbc4afe867495df7ef27
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-21 00:46:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,10 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   out = []
-  holiday_hash.each_value{|season| puts season}
+  holiday_hash.each_value{|season| 
+    season.each_value{ |holiday|
+      puts holiday
+    }}
 end
 
 puts all_holidays_with_bbq(
